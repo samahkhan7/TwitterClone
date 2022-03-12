5b5b7d06d375d3bf15449b7c1bd2b3bd14dbbdf6
Samah Khan <k.samah7@gmail.com>
2022-03-11 20:25:59 -0600

    created user & tweet objects

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
@@ -0,0 +1,33 @@
+package com.codepath.apps.restclienttemplate.models
+
+import org.json.JSONArray
+import org.json.JSONObject
+
+class Tweet {
+
+    var body: String = ""
+    var createdAt: String = ""
+    var user: User? = null
+
+    companion object {
+        // pass in a json object and turn that into a tweet object we can use
+
+        fun fromJson(jsonObject: JSONObject): Tweet {
+
+            val tweet = Tweet()
+            tweet.body = jsonObject.getString("text")
+            tweet.createdAt = jsonObject.getString("created_at")
+            tweet.user = User.fromJson(jsonObject.getJSONObject("user"))
+            return tweet
+        }
+
+        // takes a whole json array and converts it into a list of tweets that is then returned
+        fun fromJsonArray(jsonArray: JSONArray): List<Tweet> {
+            val tweets = ArrayList<Tweet>()
+            for (i in 0..jsonArray.length()) {
+                tweets.add(fromJson(jsonArray.getJSONObject(i)))
+            }
+            return tweets
+        }
+    }
+}
